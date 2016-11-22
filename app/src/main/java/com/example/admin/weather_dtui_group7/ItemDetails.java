37969b6a585c0f9b6f15a55c8917eead1017f0e5
Thaopt191 <thaopt191@gmail.com>
2016-11-22 18:51:12 +0700

    backbtn

--- app/src/main/java/com/example/admin/weather_dtui_group7/ItemDetails.java
+++ app/src/main/java/com/example/admin/weather_dtui_group7/ItemDetails.java
@@ -1,7 +1,9 @@
 package com.example.admin.weather_dtui_group7;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class ItemDetails extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class ItemDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_details);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                // app icon in action bar clicked; go home
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
