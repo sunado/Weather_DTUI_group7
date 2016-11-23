5e6dd1853fa5ee182e3656456385590dfcf9fe84
phonga3cvp <phonga3cvp@gmail.com>
2016-11-24 01:07:00 +0700

    edit-navigation

--- app/src/main/java/com/example/admin/weather_dtui_group7/MainActivity.java
+++ app/src/main/java/com/example/admin/weather_dtui_group7/MainActivity.java
@@ -20,7 +20,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,40 +38,22 @@ public class MainActivity extends FragmentActivity {
         mFragmentManager = getSupportFragmentManager();
         mFragmentTransaction = mFragmentManager.beginTransaction();
         mFragmentTransaction.replace(R.id.containerView,new TabFragment()).commit();
-        /**
-         * Setup click events on the Navigation View Items.
-         */
 
-        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(MenuItem menuItem) {
-                mDrawerLayout.closeDrawers();
-
-
-
-                if (menuItem.getItemId() == R.id.nav_item_sent) {
-                    FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
-                    fragmentTransaction.replace(R.id.containerView,new NowFragment()).commit();
-
-                }
-
-                if (menuItem.getItemId() == R.id.nav_item_inbox) {
-                    FragmentTransaction xfragmentTransaction = mFragmentManager.beginTransaction();
-                    xfragmentTransaction.replace(R.id.containerView,new TabFragment()).commit();
-                }
-
-                return false;
-            }
-
-        });
 
         /**
          * Setup Drawer Toggle of the Toolbar
          */
 
         android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this,mDrawerLayout, toolbar,R.string.app_name,
-                R.string.app_name);
+                R.string.app_name){
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                View container = findViewById(R.id.containerView);
+                container.setTranslationX(slideOffset * drawerView.getWidth());
+            }
+        };
 
         mDrawerLayout.setDrawerListener(mDrawerToggle);
 
