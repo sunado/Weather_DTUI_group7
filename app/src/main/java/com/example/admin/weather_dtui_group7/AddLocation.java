69a5d4485bd6e49b7442146c96c175631ff08d4b
phonga3cvp <phonga3cvp@gmail.com>
2016-11-25 14:44:24 +0700

    custom search

--- app/src/main/java/com/example/admin/weather_dtui_group7/AddLocation.java
+++ app/src/main/java/com/example/admin/weather_dtui_group7/AddLocation.java
@@ -7,13 +7,18 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import pl.droidsonroids.gif.GifTextView;
+
 public class AddLocation extends AppCompatActivity {
     String[] location = new String[]{"Hà Nội,Hà Nội,(VN)","New York, (US)","San Francisco,California,(US)",
-            "Hải Phòng, Việt Nam","Tokyo, (JP)", "Hirosima, (JP)"};
+            "Hải Phòng, Việt Nam","Tokyo, (JP)"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +28,6 @@ public class AddLocation extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,location){
             @NonNull
             @Override
@@ -32,11 +36,31 @@ public class AddLocation extends AppCompatActivity {
 
                 TextView textView=(TextView) view.findViewById(android.R.id.text1);
                 textView.setTextColor(Color.WHITE);
-
+                textView.setTextSize(12);
+                textView.setBackgroundColor(Color.parseColor("#696161"));
                 return view;
             }
         };
-
+        listView.setCacheColorHint(Color.parseColor("#696161"));
         listView.setAdapter(adapter);
+
+        AutoCompleteTextView text =(AutoCompleteTextView)findViewById(R.id.auto);
+        ArrayAdapter adapter_auto = new
+                ArrayAdapter(this,android.R.layout.simple_list_item_1,location){
+                    @NonNull
+                    @Override
+                    public View getView(int position, View convertView, ViewGroup parent) {
+                        View view =super.getView(position, convertView, parent);
+
+                        TextView textView=(TextView) view.findViewById(android.R.id.text1);
+                        textView.setTextColor(Color.WHITE);
+                        textView.setTextSize(12);
+                        textView.setBackgroundColor(Color.parseColor("#696161"));
+                        return view;
+                    }
+                };
+
+        text.setAdapter(adapter_auto);
+        text.setThreshold(1);
     }
 }
